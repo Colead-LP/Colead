27fbe095c2874e12b161b3e1f33a147c0fb672b0
shigeta <x.yusuke.x@icloud.com>
2022-10-10 21:52:31 +0900

    footerとちゅう

--- index.php
+++ index.php
@@ -27,11 +27,10 @@
                     </div>
                 </div>
             </header><!-- /.header -->
+            <!-- mv -->
             <div class="mv-inner inner">
                 <div class="mv-txt">
-                    <p class="txt txt01">最短<span class="txt__strong txt__midium"><span
-                                class="txt__large">1</span>週間</span>でお振り込みが可能
-                    </p>
+                    <p class="txt txt01">最短<span class="txt__strong txt__midium"><span class="txt__large">1</span>週間</span>でお振り込みが可能</p>
                     <p class="txt txt02">太陽光発電所の</p>
                     <p class="txt txt03">スピード買取</p>
                 </div>
@@ -44,7 +43,52 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div><!-- /.mv -->
+
+    <!-- solve -->
+    <section class="solve">
+
+    </section><!-- /.solve -->
+
+    <!-- cta -->
+    <div class="cta"></div><!-- /.cta -->
+
+    <!-- reason -->
+    <section class="reason">
+
+    </section><!-- /.reason -->
+
+    <!-- cta -->
+    <div class="cta"></div><!-- /.cta -->
+
+    <!-- flow -->
+    <section class="flow"></section><!-- /.flow -->
+
+    <!-- faq -->
+    <section class="faq"></section><!-- faq -->
+
+    <!-- cta -->
+    <section class="cta"></section><!-- /.cta -->
+
+    <!-- form -->
+    <section class="form">
+
+    </section><!-- /.form -->
+
+    <!-- footer -->
+    <footer class="footer">
+        <div class="footer-bg">
+            <div class="footer-inner inner">
+                <h2 class="footer-ttl"><img src="./assets/img/footer-logo.png" alt=""></h2>
+                <div class="footer-txt">
+                    <p class="txt">株式会社Colead</p>
+                    <p class="txt">東京都千代田区平河町1-8-2</p>
+                </div>
+                <p class="copyright">Copyright © Colead Allrights Reserved.</p>
+            </div>
+        </div>
+    </footer><!-- /.footer -->
+
 </body>
 
 </html>
