da8bed3c5fe6ebdcb62c8b3dcdbe339bc37b2d4c
shigeta <x.yusuke.x@icloud.com>
2022-10-10 14:51:29 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Colead LP_TOP</title>
+</head>
+
+<body>
+    <!-- header -->
+    <header class="header">
+        <div class="header-inner">
+            <h1 class="header-logo">
+                <img src="./assets/img/logo.svg" alt="">
+            </h1>
+            <p class="header-txt">
+                太陽光発電所の<span class="sp-br"></span>
+                スピード買取ならコリード
+            </p>
+        </div>
+    </header><!-- /.header -->
+
+    <div class="mv">
+        <div class="mv-bg">
+            <div class="mv-inner">
+                <div class="mv-txt">
+                    <p class="txt txt01">最短<span class="txt__strong"><span class="txt__large">1</span>週間</span>でお振り込みが可能</p>
+                    <p class="txt txt02">太陽光発電所の</p>
+                    <p class="txt txt03">スピード買取</p>
+                </div>
+                <div class="mv-label">
+                    <p class="label label01">手数料無料</p>
+                    <p class="label label02">買取実績多数</p>
+                </div>
+                <div class="mv-btn">
+                    <a href="#" class="btn"><img src="./assets/img/btn.to-simulation.png" alt="無料金額シミュレーションボタン"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
