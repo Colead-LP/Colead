ee8d612dcfcab4d10373c165e5dc8bbca8b050e8
shigeta <x.yusuke.x@icloud.com>
2022-10-15 15:48:33 +0900

    reason途中

--- front-page.php
+++ front-page.php
@@ -76,7 +76,7 @@
         <div class="solve-footer">
             <div class="solve-footer-bg">
                 <div class="solve-footer-inner inner">
-                    <h3 class="solve-footer-ttl">最短<span class="txt__strong"><span class="txt__45">1</span>週間</span>で決済可能。</h3>
+                    <h3 class="solve-footer-ttl"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/solve/sp/footer-ttl.png" alt=""></h3>
                     <p class="solve-footer-txt">スムーズな売却でストレスフリー。</p>
                     <p class="solve-footer-txt">太陽光発電の買取なら<ruby>Colead<rt>コリード</rt></ruby>にご相談ください。自社で買い取るのでスピーディ。太陽光発電事業に精通したプロフェッショナルがワンストップで対応するので手続きもスムーズです。</p>
                     <div class="solve-footer-graph">
@@ -107,7 +107,39 @@
 
     <!-- reason -->
     <section class="reason">
-
+        <div class="reason-bg">
+            <div class="reason-inner inner">
+                <h2 class="reason-ttl"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/reason/sp/ttl.png" alt=""></h2>
+                <ul class="reason-list">
+                    <li class="reason-item">
+                        <div class="reason-item-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/reason/sp/item-img01.png" alt=""></div>
+                        <h3 class="reason-item-ttl">直接買取だから現金化までが
+                            スピーディ。手数料も0円</h3>
+                        <p class="reason-item-txt">弊社は仲介業者ではなく、太陽光発電事業者として自社買取を行います。
+                            そのため、発電所の購入完了までの時間が非常に短いことが大きな特徴です。</p>
+                    </li>
+                    <li class="reason-item">
+                        <div class="reason-item-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/reason/sp/item-img02.png" alt=""></div>
+                        <h3 class="reason-item-ttl">太陽光投資のプロがワンストップで対応。手続きもスムーズ</h3>
+                        <p class="reason-item-txt">弊社では開発に関与した太陽光発電所が合計10MWを超える実務実績を保有しております。発電所売買に関するノウハウを基に担当者が即断即決で進めるため、意思決定が速いことが特徴です。
+                            初めて発電所を売却される方も丁寧にリードいたします。</p>
+                    </li>
+                    <li class="reason-item">
+                        <div class="reason-item-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/reason/sp/item-img03.png" alt=""></div>
+                        <h3 class="reason-item-ttl">低圧から高圧まで即時購入可能
+                            な豊富な資金を保有</h3>
+                        <p class="reason-item-txt">弊社では基本的に現金にて発電所を購入いたします。銀行融資の進捗状況によって購入が遅れるといった事態はございません。</p>
+                    </li>
+                    <li class="reason-item">
+                        <div class="reason-item-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/reason/sp/item-img04.png" alt=""></div>
+                        <h3 class="reason-item-ttl">全国の発電所が買取対象。
+                            現地調査も対応</h3>
+                        <p class="reason-item-txt">抑制エリアでも喜んで購入いたします。また、他の査定会社に購入を断られた発電所でも購入の可能性はあります。
+                            本当に売却できるか悩ましい発電所であっても、是非一度お声がけください。</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </section><!-- /.reason -->
 
     <!-- cta -->
