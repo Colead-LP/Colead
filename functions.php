4fd7b5f18849b1f996c83f2282cad0560d93dace
shigeta <x.yusuke.x@icloud.com>
2022-10-20 16:37:08 +0900

    functionsphp修正

--- functions.php
+++ functions.php
@@ -3,15 +3,15 @@
 function add_my_files()
 {
     // CSS
-    wp_enqueue_style('reset', get_template_directory_uri() . '/css/reset.css');
     // 各ページ
     if (is_front_page()) {
-        wp_enqueue_style('top', get_template_directory_uri() . '/css/style.css');
+        wp_enqueue_style('top', get_template_directory_uri() . '/assets/css/style.css');
     } elseif (is_page('confirm')) {
-        wp_enqueue_style('confirm', get_template_directory_uri() . '/css/confirm.css');
+        wp_enqueue_style('confirm', get_template_directory_uri() . '/assets/css/style.css');
     } elseif (is_page('thanks')) {
-        wp_enqueue_style('thanks', get_template_directory_uri() . '/css/thanks.css');
+        wp_enqueue_style('thanks', get_template_directory_uri() . '/assets/css/style.css');
     }
+    wp_enqueue_style('reset', get_template_directory_uri() . '/assets/css/reset.css');
 
     //JavaScript
     // wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
