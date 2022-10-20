e9067ad0293f6c336d927fdbb363a0f2b9336e46
ryorgyo <ryorgyo@icloud.com>
2022-10-20 16:15:29 +0900

    一時保存

--- page-thanks.php
+++ page-thanks.php
@@ -15,24 +15,6 @@ Template Name: thanks
 </head>
 
 <body>
-    <!-- header -->
-    <div class="mv">
-        <div class="mv-bg">
-            <header class="header">
-                <div class="header-bg">
-                    <div class="header-inner inner">
-                        <h1 class="header-logo">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/sp/logo.svg" alt="">
-                        </h1>
-                        <p class="header-txt">
-                            太陽光発電所の<span class="sp-br"></span>
-                            スピード買取ならコリード
-                        </p>
-                    </div>
-                </div>
-            </header><!-- /.header -->
-        </div>
-    </div>
 
     <main class='main'>
         <div class='main-inner'>
@@ -41,17 +23,5 @@ Template Name: thanks
         </div>
     </main>
 
-    <footer class="footer">
-        <div class="footer-bg">
-            <div class="footer-inner inner">
-                <h2 class="footer-ttl"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/footer-logo.png" alt=""></h2>
-                <div class="footer-txt">
-                    <p class="txt">株式会社Colead</p>
-                    <p class="txt">東京都千代田区平河町1-8-2</p>
-                </div>
-                <p class="copyright">Copyright © Colead Allrights Reserved.</p>
-            </div>
-        </div>
-    </footer>
 </body>
 </html>
