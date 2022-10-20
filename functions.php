35600779888cd102cd3dca5193837ccbc2014786
shigeta <x.yusuke.x@icloud.com>
2022-10-20 16:58:21 +0900

    a

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function add_my_files()
 {
     // CSS
     // 各ページ
+    wp_enqueue_style('reset', get_template_directory_uri() . '/assets/css/reset.css');
     if (is_front_page()) {
         wp_enqueue_style('top', get_template_directory_uri() . '/assets/css/style.css');
     } elseif (is_page('confirm')) {
@@ -11,7 +12,6 @@ function add_my_files()
     } elseif (is_page('thanks')) {
         wp_enqueue_style('thanks', get_template_directory_uri() . '/assets/css/style.css');
     }
-    wp_enqueue_style('reset', get_template_directory_uri() . '/assets/css/reset.css');
 
     //JavaScript
     // wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
