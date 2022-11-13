b0cc2f2c9cd96afe1502dd23dc453743a4cfa068
shigeta <x.yusuke.x@icloud.com>
2022-11-13 14:09:40 +0900

    slideToggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,16 +44,14 @@ $(function () {
     });
   }
 
-  // $(function() {
-  //   var scrollPos;//topからのスクロール位置
-  //   $('.btn').click(function() {
-  //     scrollPos = $(window).scrollTop();//topからのスクロール位置を格納
-  //     $('.content').fadeIn();//モーダルをフェードイン
-  //     $('body').addClass('fixed').css({ top: -scrollPos });//背景固定
-  //     return false;//<a>を無効化
-  //   });
-  // });
+
+  function slideToggle () {
+    $('.js-switch').on('click', function(){
+      $('.js-slideToggle').slideToggle();
+    });
+  }
 
   changeTxt();
   privacyModal();
+  slideToggle();
 });
