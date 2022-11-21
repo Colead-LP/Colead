8feea1b32badc1de5362abd8b91b43aec9e86046
shigeta <x.yusuke.x@icloud.com>
2022-11-22 08:03:11 +0900

    thanksページ作成

--- page-thanks.php
+++ page-thanks.php
@@ -1,6 +1,6 @@
-<main class='main'>
-    <div class='main-inner'>
-        <h2 class='main-inner_title'>お問い合わせ<br class='media-br'>ありがとうございました。</h2>
-        <p class='main-inner_txt'>お問い合わせいただき誠にありがとうございます。<br class='PC-br'>送信いただいた内容を確認のうえ、担当者よりご連絡させていただきます。<br class='PC-br'>今しばらくお待ちください。</p>
+<main class='thanks'>
+    <div class='thanks_inner'>
+        <h2 class='thanks_ttl'>お問い合わせ<span class="sp-br"></span>ありがとうございました。</h2>
+        <p class='thanks_txt'>お問い合わせいただき誠にありがとうございます。<span class="pc-br"></span>送信いただいた内容を確認のうえ、担当者よりご連絡させていただきます。<span class="pc-br"></span>今しばらくお待ちください。</p>
     </div>
 </main>
