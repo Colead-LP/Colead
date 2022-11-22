43ccbaafbe74bf34ba6b6871086cb28b088b2335
shigeta <x.yusuke.x@icloud.com>
2022-11-23 02:29:13 +0900

    画像変更

--- templates/cta.php
+++ templates/cta.php
@@ -6,12 +6,13 @@
                 <p class="txt">太陽光発電所<span class="txt__m">の</span>スピード買取<span class="txt__s">なら</span></p>
             </div>
             <div class="cta-logo">
-                <img class="sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/cta/sp/logo.png" alt="">
-                <img class="pc" src="<?php echo get_template_directory_uri(); ?>/assets/img/cta/pc/logo.png" alt="">
+                <!-- <img class="sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/cta/sp/logo.png" alt=""> -->
+                <img class="sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/sp.cta_logo.png" alt="">
+                <img class="pc" src="<?php echo get_template_directory_uri(); ?>/assets/img/pc.cta_logo.png" alt="">
             </div>
             <a href="#contact" class="cta-btn btn">
-                <img class="sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/common/sp/btn.to-simulation.png" alt="">
-                <img class="pc" src="<?php echo get_template_directory_uri(); ?>/assets/img/common/pc/btn.to-simulation.png" alt="">
+                <img class="sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/sp.btn.to_simulation.png" alt="">
+                <img class="pc" src="<?php echo get_template_directory_uri(); ?>/assets/img/pc.btn.to_simulation.png" alt="">
             </a>
         </div>
     </div>
