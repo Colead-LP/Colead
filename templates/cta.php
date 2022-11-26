258672d9792cecb2b269eea7a5383d39a4814549
shigeta <x.yusuke.x@icloud.com>
2022-11-26 14:14:58 +0900

    ・alt・テキストアンダーラインスマホに対応

--- templates/cta.php
+++ templates/cta.php
@@ -6,13 +6,12 @@
                 <p class="txt">太陽光発電所<span class="txt__m">の</span>スピード買取<span class="txt__s">なら</span></p>
             </div>
             <div class="cta-logo">
-                <!-- <img class="sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/cta/sp/logo.png" alt=""> -->
-                <img class="sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/sp.cta_logo.png" alt="">
-                <img class="pc" src="<?php echo get_template_directory_uri(); ?>/assets/img/pc.cta_logo.png" alt="">
+                <img class="sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/sp.cta_logo.png" alt="Colead">
+                <img class="pc" src="<?php echo get_template_directory_uri(); ?>/assets/img/pc.cta_logo.png" alt="Colead">
             </div>
             <a href="#contact" class="cta-btn btn">
-                <img class="sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/sp.btn.to_simulation.png" alt="">
-                <img class="pc" src="<?php echo get_template_directory_uri(); ?>/assets/img/pc.btn.to_simulation.png" alt="">
+                <img class="sp" src="<?php echo get_template_directory_uri(); ?>/assets/img/sp.btn.to_simulation.png" alt="無料金額シミュレーション">
+                <img class="pc" src="<?php echo get_template_directory_uri(); ?>/assets/img/pc.btn.to_simulation.png" alt="無料金額シミュレーション">">
             </a>
         </div>
     </div>
